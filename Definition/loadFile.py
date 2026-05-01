9f0babdab9a4e487ef146aec442068cc7e789b53
erickaloz <elozon@nrel.gov>
2026-05-01 11:04:04 -0600

    small change to loadFile

--- Definition/loadFile.py
+++ Definition/loadFile.py
@@ -13,8 +13,7 @@ input_file = os.path.join(dir,'task49_deepwater_array.yaml')
 project = Project(file=input_file,raft=True)
 
 # plot 2D with cable labels (this may help with debugging the cable routing)
-settings = {'cable_labels': True}
-project.plot2d(plot_bathymetry = False, **settings)
+project.plot2d(plot_bathymetry = False, cable_labels = True)
 
 # plot 2D
 project.plot2d(plot_bathymetry = False)
